98fe03eaa22168e36252d47e34ea01b52fea207a
Lalith Narayan <26842636+lalith1403@users.noreply.github.com>
2020-09-04 12:32:55 +0530

    Added article on huffman coding

---
+++ articles/huffman-coding-in-python/encode.py
@@ -0,0 +1,123 @@
+import math
+import sys
+global probabilities
+probabilities = []
+
+
+class HuffmanCode:
+    def __init__(self, probability):
+        self.probability = probability
+
+    def position(self, value, index):
+        for j in range(len(self.probability)):
+            if(value >= self.probability[j]):
+                return j
+        return index-1
+
+    def characteristics_huffman_code(self, code):
+        length_of_code = [len(k) for k in code]
+
+        mean_length = sum(
+            [a*b for a, b in zip(length_of_code, self.probability)])
+
+        variance_of_code = 0
+        for k in range(len(length_of_code)):
+            variance_of_code = variance_of_code + \
+                ((length_of_code[k]-mean_length)**2)*P[k]
+
+        entropy_of_code = 0
+        for j in self.probability:
+            entropy_of_code = entropy_of_code + j*(math.log(1/j))/math.log(2)
+
+        print("Average length: %f" % mean_length)
+        # print("Entropy: %f" % entropy_of_code)
+        # print("Variance: %f" % variance_of_code)
+        print("Efficiency: %f" % (entropy_of_code/mean_length))
+
+    def compute_code(self):
+        num = len(self.probability)
+        huffman_code = ['']*num
+
+        for i in range(num-2):
+            val = self.probability[num-i-1] + self.probability[num-i-2]
+            if(huffman_code[num-i-1] != '' and huffman_code[num-i-2] != ''):
+                huffman_code[-1] = ['1' +
+                                    symbol for symbol in huffman_code[-1]]
+                huffman_code[-2] = ['0' +
+                                    symbol for symbol in huffman_code[-2]]
+            elif(huffman_code[num-i-1] != ''):
+                huffman_code[num-i-2] = '0'
+                huffman_code[-1] = ['1' +
+                                    symbol for symbol in huffman_code[-1]]
+            elif(huffman_code[num-i-2] != ''):
+                huffman_code[num-i-1] = '1'
+                huffman_code[-2] = ['0' +
+                                    symbol for symbol in huffman_code[-2]]
+            else:
+                huffman_code[num-i-1] = '1'
+                huffman_code[num-i-2] = '0'
+
+            position = self.position(val, i)
+            probability = self.probability[0:(len(self.probability) - 2)]
+            probability.insert(position, val)
+            if(isinstance(huffman_code[num-i-2], list) and isinstance(huffman_code[num-i-1], list)):
+                complete_code = huffman_code[num-i-1] + huffman_code[num-i-2]
+            elif(isinstance(huffman_code[num-i-2], list)):
+                complete_code = huffman_code[num-i-2] + [huffman_code[num-i-1]]
+            elif(isinstance(huffman_code[num-i-1], list)):
+                complete_code = huffman_code[num-i-1] + [huffman_code[num-i-2]]
+            else:
+                complete_code = [huffman_code[num-i-2], huffman_code[num-i-1]]
+
+            huffman_code = huffman_code[0:(len(huffman_code)-2)]
+            huffman_code.insert(position, complete_code)
+
+        huffman_code[0] = ['0' + symbol for symbol in huffman_code[0]]
+        huffman_code[1] = ['1' + symbol for symbol in huffman_code[1]]
+
+        if(len(huffman_code[1]) == 0):
+            huffman_code[1] = '1'
+
+        count = 0
+        final_code = ['']*num
+
+        for i in range(2):
+            for j in range(len(huffman_code[i])):
+                final_code[count] = huffman_code[i][j]
+                count += 1
+
+        final_code = sorted(final_code, key=len)
+        return final_code
+
+
+string = input("Enter the string to compute Huffman Code: ")
+
+freq = {}
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+length = len(string)
+
+probabilities = [float("{:.2f}".format(frequency[1]/length))
+                 for frequency in freq]
+probabilities = sorted(probabilities, reverse=True)
+
+huffmanClassObject = HuffmanCode(probabilities)
+P = probabilities
+
+huffman_code = huffmanClassObject.compute_code()
+
+print(' Char | Huffman code ')
+print('----------------------')
+
+for id, char in enumerate(freq):
+    if huffman_code[id] == '':
+        print(' %-4r |%12s' % (char[0], 1))
+        continue
+    print(' %-4r |%12s' % (char[0], huffman_code[id]))
+
+huffmanClassObject.characteristics_huffman_code(huffman_code)
