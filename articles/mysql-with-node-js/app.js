9ab7abb60048c12df90dc49c9d41b6649dd7ec7a
kim <kimkimani016@gmail.com>
2020-12-20 11:11:38 +0300

    Delete app.js

--- articles/mysql-with-node-js/app.js
+++
@@ -1,7 +0,0 @@
-let express = require("express");
-let mysql = require("mysql");
-let app = express();
-
-app.listen(3000, () => {
-  console.log("Application started and Listening on port 3000");
-});
